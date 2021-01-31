200833f4e385cee307f79a440f45a781feca545d
Aneta Di Scala <38839156+netka99@users.noreply.github.com>
2021-01-31 22:00:20 +0100

    First commit

---
+++ src/components/GameDetail.js
@@ -0,0 +1,129 @@
+import React from "react";
+//Styling and animation
+import styled from "styled-components";
+import { motion } from "framer-motion";
+//Redux
+import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { smallImage } from "../util";
+
+const GameDetail = ({ pathId }) => {
+  const history = useHistory();
+  //Exit Detail
+  const exitDetailHandler = (e) => {
+    const element = e.target;
+    if (element.classList.contains("shadow")) {
+      document.body.style.overflow = "auto";
+      history.push("/");
+    }
+  };
+
+  //Data
+  const { screen, game, isLoading } = useSelector((state) => state.detail);
+  return (
+    <>
+      {!isLoading && (
+        <CardShadow className="shadow" onClick={exitDetailHandler}>
+          <Detail layoutId={pathId}>
+            <Stats>
+              <div className="rating">
+                <motion.h3 LayoutId={`title ${pathId}`}>{game.name}</motion.h3>
+                <p>Rating: {game.rating}</p>
+              </div>
+              <Info>
+                <h3>Platforms</h3>
+                <Platforms>
+                  {game.platforms.map((data) => (
+                    <h3 key={data.platform.id}>{data.platform.name}</h3>
+                  ))}
+                </Platforms>
+                <Description>
+                  <p>{game.description_raw}</p>
+                </Description>
+              </Info>
+            </Stats>
+            <Media>
+              <motion.img
+                LayoutId={`image ${pathId}`}
+                src={smallImage(game.background_image, 1280)}
+                alt={game.background_image}
+              />
+            </Media>
+            <div className="gallery">
+              {screen.results.map((screen) => (
+                <img
+                  src={smallImage(screen.image, 1280)}
+                  key={screen.id}
+                  alt={screen.image}
+                />
+              ))}
+            </div>
+          </Detail>
+        </CardShadow>
+      )}
+    </>
+  );
+};
+
+const CardShadow = styled(motion.div)`
+  width: 100%;
+  min-height: 100vh;
+  overflow-y: scroll;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  top: 0;
+  left: 0;
+  &::-webkit-scrollbar {
+    width: 0.75rem;
+  }
+  &::-webkit-scrollbar-thumb {
+    background-color: #ff7676;
+  }
+  &::-webkit-scrollbar-track {
+    background: white;
+  }
+`;
+
+const Detail = styled(motion.div)`
+  width: 80%;
+  border-radius: 1rem;
+  padding: 2rem 5rem;
+  background: white;
+  position: absolute;
+  left: 10%;
+  color: black;
+  img {
+    width: 100%;
+  }
+`;
+
+const Stats = styled(motion.div)`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Info = styled(motion.div)`
+  text-align: center;
+`;
+
+const Platforms = styled(motion.div)`
+  display: flex;
+  justify-content: space-evenly;
+  img {
+    margin-left: 3rem;
+  }
+`;
+
+const Media = styled(motion.div)`
+  margin-top: 5rem;
+  img {
+    width: 100%;
+  }
+`;
+
+const Description = styled(motion.div)`
+  margin: 5rem 0rem;
+`;
+
+export default GameDetail;
---
+++ src/util.js
@@ -0,0 +1,28 @@
+//Media Resize
+
+export const smallImage = (imagePath, size) => {
+  // if(imagePath){
+  // const image = imagePath.match(/media\/screenshots/)
+  //   ? imagePath.replace(
+  //       "media/screenshots",
+  //       `media/resize/${size}/-/screenshots`
+  //     )
+  //   : imagePath.replace("/media/games", `/media/resize/${size}/games/`);
+  // return image;
+  // }
+  // return imagePath
+
+  let image;
+  if (imagePath) {
+    image = imagePath.match(/media\/screenshots/)
+      ? imagePath.replace(
+          "media/screenshots",
+          `media/resize/${size}/-/screenshots`
+        )
+      : imagePath.replace("/media/games", `/media/resize/${size}/-/games`);
+  } else {
+    image = imagePath;
+  }
+
+  return image;
+};
